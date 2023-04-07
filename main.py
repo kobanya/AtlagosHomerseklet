765cea42597859739ad430c2ccda031dcfbf0dbf
kobanya <nagy.bela.budapest@gmail.com>
2023-04-07 13:42:51 +0200

    legalacsonyabb és legmagasabb érték

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# NAGY Béla 2023.04.12
 import random
 import tkinter as tk
 from tkinter import ttk
@@ -10,6 +11,9 @@ honap30 = ['Április', 'Június', 'Szeptember', 'November']
 honap31 = ['Január', 'Március', 'Május', 'Július', 'Augusztus', 'Október', 'December']
 napok_szama = 1
 atlag_max = 0
+
+'''legkis_label = None
+legnagy_label = None'''
 
 
 def generalas():
@@ -49,24 +53,24 @@ def szamitas(napok_szama):
 
 # átlagok kiszámítása
 def atlagok():
-    global atlag_max, atlag_min
-    atlag_max = sum(max_lista) / napok_szama
-    atlag_min = sum(min_lista) / napok_szama
-    return f'Az átlagos minimum hőmérséklet: {atlag_min:.2f} °C\nAz átlagos maximum hőmérséklet: {atlag_max:.2f} °C'
-
-
-atlagok()
+    global atlag_max, atlag_min, legalacsonyabb, legmagasabb, legkis_label, legnagy_label
+    atlag_max = sum(max_lista) / napok_szama  # a lista öszege osztva a napok számával
+    atlag_min = sum(min_lista) / napok_szama  # a lista öszege osztva a napok számával
+    legalacsonyabb = min(min_lista)
+    legmagasabb = max(max_lista)
+    return atlag_min, atlag_max, legalacsonyabb, legmagasabb
 
 
 def uj_adatok():
-    global napok_szama, honap_neve, nap_ma, min_lista, max_lista, atlag_min_label, atlag_max_label
+    global napok_szama, honap_neve, nap_ma, min_lista, max_lista, atlag_min_label, atlag_max_label, legkis_label, legnagy_label
     generalas()
     atlagok()
+
     kulonbseg = szamitas(napok_szama)
     tablazat.delete(*tablazat.get_children())
 
     honap_neve_str = "".join(honap_neve)
-    cimke2.config(text="A hónap neve: " + honap_neve_str)
+    cimke2.config(text=f"A hónap neve: {honap_neve_str:12}")
 
     napok_szama_str = str(napok_szama)
     cimke3.config(text="A napok száma: " + napok_szama_str)
@@ -74,8 +78,12 @@ def uj_adatok():
     atlag_min_label.config(text="Az átlagos minimum hőmérséklet: {:.2f} °C".format(atlag_min))
     atlag_max_label.config(text="Az átlagos maximum hőmérséklet: {:.2f} °C".format(atlag_max))
 
-    atlag_min_label.pack(side=tk.LEFT, padx=10, pady=5)
-    atlag_max_label.pack(side=tk.LEFT, padx=10, pady=5)
+    legalacsonyabb = min(min_lista)
+    legmagasabb = max(max_lista)
+
+    # Frissítjük a legkis_label és legnagy_label szövegét
+    legkis_label.config(text="A legalacsonyabb hőmérséklet: {:.2f} °C".format(legalacsonyabb))
+    legnagy_label.config(text="A legmagasabb hőmérséklet: {:.2f} °C".format(legmagasabb))
 
     for i in range(napok_szama):
         if i < napok_szama:
@@ -84,13 +92,12 @@ def uj_adatok():
         else:
             tablazat.insert(parent="", index=i, values=("", "", "", "", ""))
 
-
-# _____________________________ GUI ______________________________________________________
+    # ======================   GUI ===========================================
 
 
 # GUI létrehozása
 root = tk.Tk()
-root.geometry("1200x500")  # a felület, ablak mérete
+root.geometry("1200x600")  # a felület, ablak mérete
 root.title("Hőingadozás")  # az ablak neve
 
 # Címke hozzáadása
@@ -123,11 +130,15 @@ napok_szama_str = str(napok_szama)
 alatta_frame = tk.Frame(root)
 alatta_frame.grid(row=2, column=0, padx=10, pady=10)
 
+# Frame létrehozása a táblázat alatt
+masodik_frame = tk.Frame(root)
+masodik_frame.grid(row=3, column=0, padx=10, pady=10)
+
 # Button elhelyezése a Frame-en belül
 uj_adatok_gomb = tk.Button(alatta_frame, text="Új adatok generalasa", command=uj_adatok)
 uj_adatok_gomb.pack(side=tk.RIGHT, padx=5, pady=5)
 
-# Label-ek elhelyezése a Frame-en belül
+# Címkék elhelyezése a Frame-en belül
 cimke2 = tk.Label(alatta_frame, text="A hónap neve: " + honap_neve_str)
 cimke2.pack(side=tk.LEFT, padx=5, pady=5)
 
@@ -140,5 +151,10 @@ atlag_min_label = tk.Label(alatta_frame, text="Az átlagos minimum hőmérsékle
 atlag_max_label = tk.Label(alatta_frame, text="Az átlagos maximum hőmérséklet: {:.2f} °C".format(atlag_max))
 atlag_min_label.pack(side=tk.LEFT, padx=10, pady=5)
 atlag_max_label.pack(side=tk.LEFT, padx=10, pady=5)
+
+legkis_label = tk.Label(masodik_frame, text="A legalacsonyabb hőmérséklet: {:.2f} °C".format(legalacsonyabb))
+legnagy_label = tk.Label(masodik_frame, text="A legmagasabb hőmérséklet: {:.2f} °C".format(legmagasabb))
+legkis_label.pack(side=tk.LEFT, padx=10, pady=5)
+legnagy_label.pack(side=tk.LEFT, padx=10, pady=5)
 
 root.mainloop()
